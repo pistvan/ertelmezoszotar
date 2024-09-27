c22065aa2879e3bb19a1e1d030c72ee5b930f48a
Istvan Palinkas <pistvan@pistvan.hu>
2024-09-27 15:19:15 +0200

    add SASS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  css: ['~/assets/css/main.scss'],
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
 })
