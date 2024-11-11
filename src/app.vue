5ff3a02bc1d5ae5fab300422c29a0e56c1bbbc86
Istvan Palinkas <pistvan@pistvan.hu>
2024-11-11 14:58:00 +0100

    add Google Tag Manager

--- src/app.vue
+++ src/app.vue
@@ -59,6 +59,14 @@ useHead({
             async: true,
             crossorigin: 'anonymous',
         }] : []),
+        // Google Tag Manager, if the container ID is set.
+        ...(process.env.GOOGLE_TAG_MANAGER_CONTAINER_ID ? [{
+            innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER_CONTAINER_ID}');`,
+        }] : []),
     ],
 });
 
